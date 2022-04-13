1e3d4a72766945d1a2d54bed31f79a258a204ed1
TAC-01 <72425987+TAC-01@users.noreply.github.com>
2022-04-13 15:22:03 +1000

    added name

--- mySokobanSolver.py
+++ mySokobanSolver.py
@@ -1,5 +1,4 @@
-
-'''
+"""
 
     Sokoban assignment
 
@@ -24,157 +23,162 @@ Last modified by 2022-03-27  by f.maire@qut.edu.au
 - clarifiy some comments, rename some functions
   (and hopefully didn't introduce any bug!)
 
-'''
+"""
 
-# You have to make sure that your code works with 
-# the files provided (search.py and sokoban.py) as your code will be tested 
+# You have to make sure that your code works with
+# the files provided (search.py and sokoban.py) as your code will be tested
 # with these files
-import search 
+import search
 import sokoban
-
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 def my_team():
-    '''
+    """
     Return the list of the team members of this assignment submission as a list
     of triplet of the form (student_number, first_name, last_name)
-    
-    '''
-#    return [ (1234567, 'Ada', 'Lovelace'), (1234568, 'Grace', 'Hopper'), (1234569, 'Eva', 'Tardos') ]
-    raise NotImplementedError()
+
+    """
+    return [
+        (10543473, "Tarrant", "Cauchi"),
+        (00000000, "Grace", "Hopper"),
+        (00000000, "Eva", "Tardos"),
+    ]
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 def taboo_cells(warehouse):
-    '''  
+    """
     Identify the taboo cells of a warehouse. A "taboo cell" is by definition
-    a cell inside a warehouse such that whenever a box get pushed on such 
-    a cell then the puzzle becomes unsolvable. 
-    
-    Cells outside the warehouse are not taboo. It is a fail to tag an 
+    a cell inside a warehouse such that whenever a box get pushed on such
+    a cell then the puzzle becomes unsolvable.
+
+    Cells outside the warehouse are not taboo. It is a fail to tag an
     outside cell as taboo.
-    
-    When determining the taboo cells, you must ignore all the existing boxes, 
-    only consider the walls and the target  cells.  
+
+    When determining the taboo cells, you must ignore all the existing boxes,
+    only consider the walls and the target  cells.
     Use only the following rules to determine the taboo cells;
      Rule 1: if a cell is a corner and not a target, then it is a taboo cell.
-     Rule 2: all the cells between two corners along a wall are taboo if none of 
+     Rule 2: all the cells between two corners along a wall are taboo if none of
              these cells is a target.
-    
-    @param warehouse: 
+
+    @param warehouse:
         a Warehouse object with the worker inside the warehouse
 
     @return
-       A string representing the warehouse with only the wall cells marked with 
-       a '#' and the taboo cells marked with a 'X'.  
+       A string representing the warehouse with only the wall cells marked with
+       a '#' and the taboo cells marked with a 'X'.
        The returned string should NOT have marks for the worker, the targets,
-       and the boxes.  
-    '''
-    ##         "INSERT YOUR CODE HERE"    
+       and the boxes.
+    """
+    ##         "INSERT YOUR CODE HERE"
     raise NotImplementedError()
 
+    # Use itertools.combination for finding the corners
+    # adjacent to a wall between 2 corners
+    # adapt sokoban _str_
 
-    #Use itertools.combination for finding the corners
-    #adjacent to a wall between 2 corners
-    #adapt sokoban _str_
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 class SokobanPuzzle(search.Problem):
-    '''
+    """
     An instance of the class 'SokobanPuzzle' represents a Sokoban puzzle.
     An instance contains information about the walls, the targets, the boxes
     and the worker.
 
-    Your implementation should be fully compatible with the search functions of 
-    the provided module 'search.py'. 
-    
-    '''
-    
+    Your implementation should be fully compatible with the search functions of
+    the provided module 'search.py'.
+
+    """
+
     #
     #         "INSERT YOUR CODE HERE"
     #
     #     Revisit the sliding puzzle and the pancake puzzle for inspiration!
     #
-    #     Note that you will need to add several functions to 
+    #     Note that you will need to add several functions to
     #     complete this class. For example, a 'result' method is needed
     #     to satisfy the interface of 'search.Problem'.
     #
     #     You are allowed (and encouraged) to use auxiliary functions and classes
 
-    
     def __init__(self, warehouse):
         raise NotImplementedError()
 
     def actions(self, state):
         """
         Return the list of actions that can be executed in the given state.
-        
+
         """
         raise NotImplementedError
 
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+
 def check_elem_action_seq(warehouse, action_seq):
-    '''
-    
+    """
+
     Determine if the sequence of actions listed in 'action_seq' is legal or not.
-    
+
     Important notes:
       - a legal sequence of actions does not necessarily solve the puzzle.
       - an action is legal even if it pushes a box onto a taboo cell.
-        
+
     @param warehouse: a valid Warehouse object
 
     @param action_seq: a sequence of legal actions.
            For example, ['Left', 'Down', Down','Right', 'Up', 'Down']
-           
+
     @return
         The string 'Impossible', if one of the action was not valid.
            For example, if the agent tries to push two boxes at the same time,
                         or push a box into a wall.
-        Otherwise, if all actions were successful, return                 
+        Otherwise, if all actions were successful, return
                A string representing the state of the puzzle after applying
                the sequence of actions.  This must be the same string as the
                string returned by the method  Warehouse.__str__()
-    '''
-    
+    """
+
     ##         "INSERT YOUR CODE HERE"
-    
+
     raise NotImplementedError()
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+
 def solve_weighted_sokoban(warehouse):
-    '''
+    """
     This function analyses the given warehouse.
-    It returns the two items. The first item is an action sequence solution. 
+    It returns the two items. The first item is an action sequence solution.
     The second item is the total cost of this action sequence.
-    
-    @param 
+
+    @param
      warehouse: a valid Warehouse object
 
     @return
-    
-        If puzzle cannot be solved 
+
+        If puzzle cannot be solved
             return 'Impossible', None
-        
-        If a solution was found, 
-            return S, C 
+
+        If a solution was found,
+            return S, C
             where S is a list of actions that solves
             the given puzzle coded with 'Left', 'Right', 'Up', 'Down'
             For example, ['Left', 'Down', Down','Right', 'Up', 'Down']
             If the puzzle is already in a goal state, simply return []
             C is the total cost of the action sequence C
 
-    '''
-    
+    """
+
     raise NotImplementedError()
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
