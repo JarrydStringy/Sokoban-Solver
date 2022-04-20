d9e472255b0c1c0b16858d1c9d24512abde6712c
TAC-01 <72425987+TAC-01@users.noreply.github.com>
2022-04-20 17:18:33 +1000

    Broke up check_elem_seq into functions. SokobanPuzzle actions, result, goal_test completed NOT tested

--- mySokobanSolver.py
+++ mySokobanSolver.py
@@ -28,7 +28,10 @@ Last modified by 2022-03-27  by f.maire@qut.edu.au
 # You have to make sure that your code works with
 # the files provided (search.py and sokoban.py) as your code will be tested
 # with these files
+from asyncore import read
 from math import floor
+
+from sqlalchemy import false, true
 
 import search
 import sokoban
@@ -75,24 +78,24 @@ def taboo_cells(warehouse):
        The returned string should NOT have marks for the worker, the targets,
        and the boxes.
     """
-    
-    #Cells in the grid
+
+    # Cells in the grid
     taboo = 'X'
     wall = '#'
-    targets = ['.', '!', '*']    
+    targets = ['.', '!', '*']
     unwanted = ['$', '@']
-    
-    #Can use itertools.combination for finding the corners
-    #Checks if next to a wall on left/right and up/down
+
+    # Can use itertools.combination for finding the corners
+    # Checks if next to a wall on left/right and up/down
     def Corner(warehouse, x, y, wall=False):
         vertWalls = 0
         horizWalls = 0
         wall
-        #Check for walls above and below
+        # Check for walls above and below
         for (dx, dy) in [(0, 1), (0, -1)]:
             if warehouse[y + dy][x + dx] == wall:
                 vertWalls += 1
-        #Check for walls left and right
+        # Check for walls left and right
         for (dx, dy) in [(1, 0), (-1, 0)]:
             if warehouse[y + dy][x + dx] == wall:
                 horizWalls += 1
@@ -100,68 +103,69 @@ def taboo_cells(warehouse):
             return (vertWalls >= 1) or (horizWalls >= 1)
         else:
             return (vertWalls >= 1) and (horizWalls >= 1)
-    
-    #RULE 1: Check if a cell is a corner and not a target
+
+    # RULE 1: Check if a cell is a corner and not a target
     def Rule1(warehouse):
-        inside = False                                  #Bool to check if inside or out
-        for y in range(len(warehouse) - 1):             #Columns
-            for x in range(len(warehouse) - 1):         #Rows
+        inside = False  # Bool to check if inside or out
+        for y in range(len(warehouse) - 1):  # Columns
+            for x in range(len(warehouse) - 1):  # Rows
                 if not inside:
-                    if warehouse[y][x] == wall:         #If we have reached the wall
-                        inside = True                   #We are inside
+                    if warehouse[y][x] == wall:  # If we have reached the wall
+                        inside = True  # We are inside
                 else:
                     if all([cell == ' ' for cell in warehouse[y][x:]]):
-                        break                           #We are back ouside
-                        
-                    if warehouse[y][x] not in targets:  #Can't be taboo if it is a target
-                        if warehouse[y][x] != wall:     #Or a wall
+                        break  # We are back ouside
+
+                    if warehouse[y][x] not in targets:  # Can't be taboo if it is a target
+                        if warehouse[y][x] != wall:  # Or a wall
                             if Corner(warehouse, x, y):
                                 warehouse[y][x] = taboo
         return warehouse
-    
-    #RULE 2: Check if there are any target cells between two corners
+
+    # RULE 2: Check if there are any target cells between two corners
     def Rule2(warehouse):
-        for y in range(1, len(warehouse) - 1):                              #Col
-            for x in range(1, len(warehouse[0]) - 1):                       #Row
-                if warehouse[y][x] == taboo and Corner(warehouse, x, y):    #Needs to be inline with a corner
+        for y in range(1, len(warehouse) - 1):  # Col
+            for x in range(1, len(warehouse[0]) - 1):  # Row
+                # Needs to be inline with a corner
+                if warehouse[y][x] == taboo and Corner(warehouse, x, y):
                     row = warehouse[y][x + 1:]
                     col = [row[x] for row in warehouse[y + 1:][:]]
-                    for x2 in range(len(row)):                              #Inner row
+                    for x2 in range(len(row)):  # Inner row
                         if row[x2] in targets or row[x2] == wall:
-                            break                                           #Can't be a target or be a wall
+                            break  # Can't be a target or be a wall
                         if row[x2] == taboo and Corner(warehouse, x2 + x + 1, y):
                             if all([Corner(warehouse, x3, y, True) for x3 in range(x + 1, x2 + x + 1)]):
                                 for x4 in range(x + 1, x2 + x + 1):
                                     warehouse[y][x4] = taboo
-                    for y2 in range(len(col)):                              #Inner col
+                    for y2 in range(len(col)):  # Inner col
                         if col[y2] in targets or col[y2] == wall:
-                            break                                           #Can't be a target or be a wall
+                            break  # Can't be a target or be a wall
                         if col[y2] == taboo and Corner(warehouse, x, y2 + y + 1):
                             if all([Corner(warehouse, x, y3, True) for y3 in range(y + 1, y2 + y + 1)]):
                                 for y4 in range(y + 1, y2 + y + 1):
                                     warehouse[y4][x] = taboo
         return warehouse
-    
-    #convert warehouse to a string
+
+    # convert warehouse to a string
     wh_str = str(warehouse)
-    
-    #Clean the string by removing the boxes and players
-    for cell in unwanted:   #If it is in list
-        wh_clean = wh_str.replace(cell, ' ')    #replace with a blank
-    
-    #Rearange string to array to show full wh in 2D array
-    wh_array = [list(line) for line in wh_clean.split('\n')]    
-    
-    #Apply the 2 rules
+
+    # Clean the string by removing the boxes and players
+    for cell in unwanted:  # If it is in list
+        wh_clean = wh_str.replace(cell, ' ')  # replace with a blank
+
+    # Rearange string to array to show full wh in 2D array
+    wh_array = [list(line) for line in wh_clean.split('\n')]
+
+    # Apply the 2 rules
     wh_array = Rule1(wh_array)
     wh_array = Rule2(wh_array)
-    
-    #Convert back into a 1D string
+
+    # Convert back into a 1D string
     wh_str = '\n'.join([''.join(line) for line in wh_array])
-    
-    #Result
+
+    # Result
     return wh_str
-    
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
@@ -188,6 +192,9 @@ class SokobanPuzzle(search.Problem):
     #     You are allowed (and encouraged) to use auxiliary functions and classes
 
     def __init__(self, warehouse):
+        if warehouse:
+            self.state = warehouse
+
         raise NotImplementedError()
 
     def actions(self, state):
@@ -195,21 +202,32 @@ class SokobanPuzzle(search.Problem):
         Return the list of actions that can be executed in the given state.
 
         """
-        raise NotImplementedError
+        L = []  # list of legal actions
+        if legal_check(state, ['Up']) != 'Impossible':
+            L.append('Up')
+        elif legal_check(state, ['Down']) != 'Impossible':
+            L.append('Down')
+        elif legal_check(state, ['Left']) != 'Impossible':
+            L.append('Left')
+        elif legal_check(state, ['Right']) != 'Impossible':
+            L.append('Right')
+
+        return L
 
     def result(self, state, action):
         """Return the state that results from executing the given
         action in the given state. The action must be one of
         self.actions(state).
         """
-
-        raise NotImplementedError
+        assert action in self.actions(state)  # defensive programming!
+        make_move(state, action)
 
     def goal_test(self, state):
-        """Return True if the state is a goal. The default method compares the
-        state to self.goal, as specified in the constructor. Override this
-        method if checking against a single self.goal is not enough."""
-        return state == self.goal
+        """Return True if all boxes in warehouse are on a target.
+        """
+        if (set(state.boxes).issubset(set(state.targets))):
+            return True
+        return False
 
     def path_cost(self, c, state1, action, state2):
         """Return the cost of a solution path that arrives at state2 from
@@ -225,6 +243,98 @@ class SokobanPuzzle(search.Problem):
         """
         raise NotImplementedError
 
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def calculate_move(warehouse, move):
+    """
+
+    Translates a give move ('Up', 'Down', 'Left', 'Right') into respective change in coordinates
+
+    """
+
+    if move == 'Up':
+        coordinate_change = (0, -1)
+    elif move == 'Down':
+        coordinate_change = (0, 1)
+    elif move == 'Left':
+        coordinate_change = (-1, 0)
+    elif move == 'Right':
+        coordinate_change = (1, 0)
+    
+    explore_tile = (warehouse.worker[0] + coordinate_change[0],
+                    warehouse.worker[1] + coordinate_change[1])  # One tile ahead
+    
+    explore_more = (explore_tile[0] + coordinate_change[0],
+                        explore_tile[1] + coordinate_change[1]) # One tile further ahead
+    
+    return explore_tile, explore_more
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def legal_check(warehouse, move):
+    """
+
+    Checks if given move is legal in given state
+
+    # LEGAL CONDITIONS:
+    # Cant walk through wall
+    # Cant push box through wall
+    # Cant push two boxes at same time
+
+    """
+    explore_tile, explore_more = calculate_move(warehouse, move)
+    
+    if binary_tuple_search(explore_tile, warehouse.walls) != -1:  # If wall
+        return 'Impossible'
+
+    if binary_tuple_search(explore_tile, warehouse.boxes) != -1:  # If box
+        if binary_tuple_search(explore_more, warehouse.walls) != -1 or binary_tuple_search(explore_more, warehouse.boxes) != -1:  # If wall OR box
+            return 'Impossible'
+        return 'box'
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+def make_move(warehouse, move):
+    """
+
+    Makes an already checked legal move in a given state
+
+    """
+    explore_tile, explore_more = calculate_move(warehouse, move)
+
+    if legal_check(warehouse, move) == 'box':
+    # Push box: Replace explore_tile with explore_more
+        warehouse.boxes[binary_tuple_search(explore_tile, warehouse.boxes)] = explore_more
+    warehouse.worker = explore_tile  # move player forward
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+def binary_tuple_search(target, list):
+    """
+    Binary Tuple Search (More efficiently searches for a given tuple in a sorted list of tuples)
+
+    @param target: a coordinate tuple E.g. (4,0)
+
+    @param list: a sorted list of coordinate tuples E.g. [(2,0),(3,0),(4,0),(0,1),(1,1)]
+
+    @return: index of target in list, -1 if not present
+    """
+
+    l = 0
+    r = len(list) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if target == list[m]:
+            return m
+        elif target[::-1] > list[m][::-1]:
+            l = m + 1
+        else:
+            r = m - 1
+    return -1
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -253,35 +363,13 @@ def check_elem_action_seq(warehouse, action_seq):
                string returned by the method  Warehouse.__str__()
     """
 
-    ## LEGAL CONDITIONS:
-    ##  Cant walk through wall
-    ##  Cant push box through wall
-    ##  Cant push two boxes at same time
-
     for move in action_seq:
-        if move == "Up":
-            coordinate_change = (0, -1)
-        elif move == "Down":
-            coordinate_change = (0, 1)
-        elif move == "Left":
-            coordinate_change = (-1, 0)
-        elif move == "Right":
-            coordinate_change = (1, 0)
-            
-        explore_tile = (warehouse.worker[0] + coordinate_change[0], warehouse.worker[1] + coordinate_change[1]) #One tile ahead
-        
-        if binary_tuple_search(explore_tile, warehouse.walls) != -1:   #If wall
+        if legal_check(warehouse, move) == 'Impossible':
             return 'Impossible'
-        
-        if binary_tuple_search(explore_tile, warehouse.boxes) != -1:   #If box
-            explore_more = (explore_tile[0] + coordinate_change[0], explore_tile[1] + coordinate_change[1])  #One tile further ahead
-            if binary_tuple_search(explore_more, warehouse.walls) != -1 or binary_tuple_search(explore_more, warehouse.boxes) != -1: #If wall OR box
-                return 'Impossible'
-            warehouse.boxes[binary_tuple_search(explore_tile, warehouse.boxes)] = explore_more #Push box: Replace explore_tile with explore_more
-        
-        warehouse.worker = explore_tile # move player forward
-    
-    return warehouse.__str__() # Return string representing state of warehouse after applying sequence of actions
+        else:
+            make_move(warehouse, move)
+    # Return string representing state of warehouse after applying sequence of actions
+    return warehouse.__str__()
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -316,76 +404,60 @@ def solve_weighted_sokoban(warehouse):
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-def binary_tuple_search(target, list):
-    """
-    Binary Tuple Search (More efficiently searches for a given tuple in an array of tuples)
-
-    @param target: a coordinate tuple E.g. (4,2)
-
-    @param list: a list of coordinate tuples E.g. [(3,6),(1,5),(4,3),(6,4),(3,5)]
-
-    @return: index of target in list, -1 if not present
-    """
-
-    l = 0
-    r = len(list) - 1
-    while l <= r:
-        m = (l + r) // 2
-        if target == list[m]:
-            return m
-        elif target[::-1] > list[m][::-1]:
-            l = m + 1
-        else:
-            r = m - 1
-    return -1
-
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 if __name__ == "__main__":
 
     wh = sokoban.Warehouse()
-    wh.load_warehouse("./warehouses/warehouse_03.txt")
 
-    ### TEST binary_tuple_search
-    # print(binary_tuple_search((0, 5), wh.walls))  # Present (Middle)
-    # print(binary_tuple_search((6, 2), wh.walls))  # Not Present
-    # print(binary_tuple_search((2, 0), wh.walls))  # Present (Start)
-    # print(binary_tuple_search((8, 5), wh.walls))  # Present (End)
+    ##### check_elem_action_seq TEST #####
 
-    ### TEST check_elem_action_seq
-    # ## UP ACTION
-    # ..... # Legal (Move to empty square)
-    # ..... # Legal (Push box)
-    # ..... # Illegal (Walk into wall)
-    # ..... # Illegal (Push box into wall)
-    # ..... # Illegal (Push two boxes)
+    # wh.load_warehouse("./warehouses/warehouse_03.txt")
+    # sequence = ['Up']
+    # print(check_elem_action_seq(wh, sequence)) # Illegal (Hits wall)
 
-    # ## DOWN ACTION
-    # ..... # Legal (Move to empty square)
-    # ..... # Legal (Push box)
-    # ..... # Illegal (Walk into wall)
-    # ..... # Illegal (Push box into wall)
-    # ..... # Illegal (Push two boxes)
+    # wh.load_warehouse("./warehouses/warehouse_03.txt")
+    # sequence = ['Down']
+    # print(check_elem_action_seq(wh, sequence)) # Illegal (Hits wall)
 
-    # ## LEFT ACTION
-    # ..... # Legal (Move to empty square)
-    # ..... # Legal (Push box)
-    # ..... # Illegal (Walk into wall)
-    # ..... # Illegal (Push box into wall)
-    # ..... # Illegal (Push two boxes)
+    # wh.load_warehouse("./warehouses/warehouse_03.txt")
+    # sequence = ['Left']
+    # print(check_elem_action_seq(wh, sequence)) # Illegal (Hits wall)
 
-    # ## RIGHT ACTION
-    # ..... # Legal (Move to empty square)
-    # ..... # Legal (Push box)
-    # ..... # Illegal (Walk into wall)
-    # ..... # Illegal (Push box into wall)
-    # ..... # Illegal (Push two boxes)
+    # wh.load_warehouse("./warehouses/warehouse_03.txt")
+    # sequence = ['Right', 'Right']
+    # print(check_elem_action_seq(wh, sequence)) # Illegal (Hits wall)
 
-    # ## MULTIPLE ACTIONS
-    # sequence1 = ["Left", "Down", "Down", "Right", "Up", "Down"]
-    # print(check_elem_action_seq(wh, sequence1)) # Legal (Push box to target)
+    # wh.load_warehouse("./warehouses/warehouse_03.txt")
+    # sequence = ['Right', 'Up', 'Up', 'Left', 'Left', 'Left', 'Down', 'Down', 'Left', 'Left', 'Left', 'Up', 'Up', 'Right', 'Right', 'Up', 'Right', 'Down', 'Down']
+    # print(check_elem_action_seq(wh, sequence)) # Legal (Push box to target)
 
-    # sequence2 = ["Right", "Up", "Up", "Left", "Left", "Left", "Down", "Down", "Left", "Left", "Left", "Up", "Up", "Right", "Right", "Up", "Right", "Down", "Down"]
-    # print(check_elem_action_seq(wh, sequence2)) # Legal (Push box to target)
+    # wh.load_warehouse("./warehouses/warehouse_03.txt")
+    # sequence = ['Right', 'Up', 'Up', 'Left', 'Left', 'Left', 'Down', 'Left', 'Up']
+    # print(check_elem_action_seq(wh, sequence)) # Legal (Push box up test)
     
+    # wh.load_warehouse("./warehouses/warehouse_03.txt")
+    # sequence = ['Right', 'Up', 'Up', 'Left', 'Left', 'Left', 'Down', 'Left', 'Up', 'Up', 'Up', 'Up']
+    # print(check_elem_action_seq(wh, sequence)) # Illegal (Push box UP into wall)
+
+    # wh.load_warehouse("./warehouses/warehouse_03.txt")
+    # sequence = ['Right']
+    # print(check_elem_action_seq(wh, sequence))  # Legal (Move to empty space)
+
+    # wh.load_warehouse("./warehouses/warehouse_03.txt")
+    # sequence = ['Right', 'Up', 'Up', 'Left', 'Left', 'Left', 'Down', 'Down', 'Left', 'Left', 'Left', 'Up', 'Up', 'Right', 'Right', 'Up', 'Right', 'Down', 'Down', 'Down']
+    # print(check_elem_action_seq(wh, sequence)) # Illegal (Push box DOWN into wall)
+
+    # wh.load_warehouse("./warehouses/warehouse_03.txt")
+    # sequence = ['Right', 'Up', 'Up', 'Left', 'Left', 'Left', 'Left', 'Left', 'Left', 'Left', 'Left', 'Left']
+    # print(check_elem_action_seq(wh, sequence)) # Illegal (Push box LEFT into wall)
+
+    # wh.load_warehouse("./warehouses/warehouse_03.txt")
+    # sequence = ['Right', 'Up', 'Up', 'Left', 'Left', 'Left', 'Down', 'Down', 'Left', 'Left', 'Left', 'Up', 'Up', 'Right', 'Right', 'Right', 'Right', 'Right', 'Right', 'Right', 'Right']
+    # print(check_elem_action_seq(wh, sequence)) # Illegal (Push box RIGHT into wall)
+
+    ###
+    # Conditions NOT tested:
+    #   - Pushing two boxes in any direction
+    ###
+
+    ##### check_elem_action_seq TEST #####
