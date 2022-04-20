e94c35b440193a0e3f9e635c3dc3331c67c32694
TAC-01 <72425987+TAC-01@users.noreply.github.com>
2022-04-20 11:40:39 +1000

    SokobanPuzzle skeleton

--- mySokobanSolver.py
+++ mySokobanSolver.py
@@ -197,6 +197,34 @@ class SokobanPuzzle(search.Problem):
         """
         raise NotImplementedError
 
+    def result(self, state, action):
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state).
+        """
+
+        raise NotImplementedError
+
+    def goal_test(self, state):
+        """Return True if the state is a goal. The default method compares the
+        state to self.goal, as specified in the constructor. Override this
+        method if checking against a single self.goal is not enough."""
+        return state == self.goal
+
+    def path_cost(self, c, state1, action, state2):
+        """Return the cost of a solution path that arrives at state2 from
+        state1 via action, assuming cost c to get up to state1. If the problem
+        is such that the path doesn't matter, this function will only look at
+        state2.  If the path does matter, it will consider c and maybe state1
+        and action. The default method costs 1 for every step in the path."""
+        return c + 1
+
+    def h(self, n):
+        """
+        Heuristic for goal state
+        """
+        raise NotImplementedError
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
